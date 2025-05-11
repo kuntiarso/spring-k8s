05dc32544dcb82377b1b41b288a20133b20ece20
Kuntiarso <gilang.grk17@gmail.com>
2025-05-11 17:05:24 +0700

    feat(common-service): add open feign log

--- commonservice/src/main/java/com/developer/superuser/commonservice/core/config/OpenFeignConfig.java
+++ commonservice/src/main/java/com/developer/superuser/commonservice/core/config/OpenFeignConfig.java
@@ -2,15 +2,18 @@ package com.developer.superuser.commonservice.core.config;
 
 import com.developer.superuser.commonservice.phoneadapter.client.PhoneFeignClient;
 import feign.Logger;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableFeignClients(basePackageClasses = {PhoneFeignClient.class})
+@Slf4j
 public class OpenFeignConfig {
     @Bean
     public Logger.Level feignLoggerLevel() {
+        log.info("Setting feign logger level to --- {}", Logger.Level.FULL.name());
         return Logger.Level.FULL;
     }
 }
